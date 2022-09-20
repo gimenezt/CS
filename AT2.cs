da292d4dfd3e5117b6378eb753d5bec8f6065e70
Lilian G <97319339+gimenezt@users.noreply.github.com>
2022-09-19 22:43:51 -0300

    Update AT2.cs

--- AT2.cs
+++ AT2.cs
@@ -1,76 +1,69 @@
-using Atividade2;
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        Funcionario f1 = new Funcionario();
-        f1.setNome("Luana");
-        f1.setMatricula(2345);
-        f1.setSalario(1800.00);
-        
-        f1.imprimir();
-        //f2.imprimir():
-        //f3.imprimir():
-        //f4.imprimir();
-        
-        Console.ReadKey();
-    }
-}
-
-//
-
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Atividade2
+namespace Juan
 {
-    class Funcionario
+    class Program
     {
-        private string Nome;
-        private int Matricula;
-        private decimal Salario;
+        static void Main(string[] args)
+        {
+         
+            Funcionario [] f1 = new Funcionario [4];
+            f1[0] = new Funcionario();
 
-        public Funcionario (string Nome, int Matricula, decimal Salario)
-        {
-            this.Nome = Nome;
-            this.Matricula = Matricula;
-            this.Salario = Salario;
-        }
-        //get e set
-        public string getNome()
-        {
-            return this.Nome;
-        }
-        public void setNome(string Nome)
-        {
-            this.Nome = Nome;
+            f1[0].setNome("Luana");
+            f1[0].setMatricula(2345);
+            f1[0].setSalario(1800);
+
+            f1.imprimir();
+            //f2.imprimir():
+            //f3.imprimir():
+            //f4.imprimir();
+
+            Console.ReadKey();
         }
 
-        public int getMatricula()
+        class Funcionario
         {
-            return this.Matricula;
-        }
-        public void setMatricula(int Matricula)
-        {
-            this.Matricula = Matricula;
-        }
+            private string Nome;
+            private int Matricula;
+            private decimal Salario;
 
-        public decimal getSalario()
-        {
-            return this.Salario;
-        }
-        public void setSalario(decimal Salario)
-        {
-            this.Salario = Salario;
-        }
+            //get e set
+            public string getNome()
+            {
+                return this.Nome;
+            }
+            public void setNome(string Nome)
+            {
+                this.Nome = Nome;
+            }
 
-        public void imprimir()
-        {
-            Console.WriteLine(this.Nome + " " + this.Matricula + " " + this.Salario);
+            public int getMatricula()
+            {
+                return this.Matricula;
+            }
+            public void setMatricula(int Matricula)
+            {
+                this.Matricula = Matricula;
+            }
+
+            public decimal getSalario()
+            {
+                return this.Salario;
+            }
+            public void setSalario(decimal Salario)
+            {
+                this.Salario = Salario;
+            }
+
+            public void imprimir()
+            {
+                Console.WriteLine(this.Nome + " " + this.Matricula + " " + this.Salario);
+            }
         }
     }
 }

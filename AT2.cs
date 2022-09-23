8c22de1482eaa81ca87118c9dce73f95761b2ee7
Lilian G <97319339+gimenezt@users.noreply.github.com>
2022-09-22 22:06:15 -0300

    Update AT2.cs

--- AT2.cs
+++ AT2.cs
@@ -1,72 +1,82 @@
-using System;
-using Atividade2;
-
 namespace Atividade2
 {
     class Program
     {
         static void Main(string[] args)
         {
+            decimal media;
             Funcionario[] f1 = new Funcionario[4];
 
             f1[0] = new Funcionario();
             f1[0].setNome("Luana");
-            f1[0].setMatricula(2345);
+            f1[0].setMatricula(3758);
             f1[0].setSalario(1800);
+            
+            f1[1] = new Funcionario();
+            f1[1].setNome("Ruan");
+            f1[1].setMatricula(9586);
+            f1[1].setSalario(3800);
 
-            f1[0].imprimir();
-            //f2.imprimir():
-            //f3.imprimir():
-            //f4.imprimir();
+            f1[2] = new Funcionario();
+            f1[2].setNome("Rodrigo");
+            f1[2].setMatricula(0284);
+            f1[2].setSalario(1500);
 
+            f1[3] = new Funcionario();
+            f1[3].setNome("Richard");
+            f1[3].setMatricula(2546);
+            f1[3].setSalario(1400);
+
+            media = (f1[0].getSalario() + f1[1].getSalario() + f1[2].getSalario() + f1[3].getSalario()) / 4;
+
+            Console.WriteLine("O(s) funcionário(s) com salário acima da média salarial é(são): ");
+
+            for(int i=0; i < 4; i++)
+            {
+                if (f1[i].getSalario() > media)
+                    f1[i].imprimir();
+            }
+            
             Console.ReadKey();
         }
     }
-}
 
-//
+    class Funcionario
+    {
+        private string Nome;
+        private int Matricula;
+        private decimal Salario;
 
-using System;
-
-namespace Atividade2
-{
-        class Funcionario
+        //get e set
+        public string getNome()
         {
-            private string Nome;
-            private int Matricula;
-            private decimal Salario;
+            return this.Nome;
+        }
+        public void setNome(string Nome)
+        {
+            this.Nome = Nome;
+        }
 
-            //get e set
-            public string getNome()
-            {
-                return this.Nome;
-            }
-            public void setNome(string Nome)
-            {
-                this.Nome = Nome;
-            }
+        public int getMatricula()
+        {
+            return this.Matricula;
+        }
+        public void setMatricula(int Matricula)
+        {
+            this.Matricula = Matricula;
+        }
 
-            public int getMatricula()
-            {
-                return this.Matricula;
-            }
-            public void setMatricula(int Matricula)
-            {
-                this.Matricula = Matricula;
-            }
-
-            public decimal getSalario()
-            {
-                return this.Salario;
-            }
-            public void setSalario(decimal Salario)
-            {
-                this.Salario = Salario;
-            }
-
-            public void imprimir()
-            {
-                Console.WriteLine(this.Nome + " " + this.Matricula + " " + this.Salario);
-            }
+        public decimal getSalario()
+        {
+            return this.Salario;
+        }
+        public void setSalario(decimal Salario)
+        {
+            this.Salario = Salario;
+        }
+                public void imprimir()
+        {
+            Console.WriteLine("Nome: " + this.Nome + ", matrícula: " + this.Matricula + ", salário: " + this.Salario);
         }
     }
+}
